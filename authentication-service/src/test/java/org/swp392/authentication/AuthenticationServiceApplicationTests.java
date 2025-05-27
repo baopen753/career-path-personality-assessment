0b31747d86fb4b80c67f8598d2ca5bf359461f15
baopen753 <tranquocbao070503@gmail.com>
2025-05-27 14:07:14 +0700

    done: initialize authentication service

---
+++ authentication-service/src/test/java/org/swp392/authentication/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swp392.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
