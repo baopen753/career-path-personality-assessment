2c6532e6f97f8a23beb050fedf30c358fda89720
baopen753 <tranquocbao070503@gmail.com>
2025-05-27 14:06:37 +0700

    done: initialize chatbox service

---
+++ chatbox-service/src/test/java/org/swp392/chatbox/ChatboxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swp392.chatbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatboxServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
