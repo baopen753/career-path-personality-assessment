88496b1c277c2741f472ca1c455624955d110e29
baopen753 <tranquocbao070503@gmail.com>
2025-05-27 14:05:36 +0700

    done: initialize university service

---
+++ university-service/src/test/java/org/swp392/universities/UniversityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swp392.universities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
