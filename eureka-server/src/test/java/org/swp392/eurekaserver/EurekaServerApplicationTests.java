d460f95bd4b0e5bdc70c8cb68fcd6c361dc8490b
baopen753 <tranquocbao070503@gmail.com>
2025-05-30 01:10:45 +0700

    add: eureka server config for eureka-server

---
+++ eureka-server/src/test/java/org/swp392/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swp392.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
