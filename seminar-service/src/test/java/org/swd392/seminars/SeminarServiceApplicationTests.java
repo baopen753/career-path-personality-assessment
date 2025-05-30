d84f41ab43ccebf766d0dcdadd44179e76dd12ea
baopen753 <tranquocbao070503@gmail.com>
2025-05-30 16:29:12 +0700

    refactor: group name in all place

---
+++ seminar-service/src/test/java/org/swd392/seminars/SeminarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swd392.seminars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminarServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ university-service/src/main/java/org/swd392/universities/UniversityServiceApplication.java
@@ -0,0 +1,13 @@
+package org.swd392.universities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityServiceApplication.class, args);
+    }
+
+}
