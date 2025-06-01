9ae9e7727fdda8e44d555b641b339c0ff45f8221
TruongTXK18FPT <truongtranxuan41@gmail.com>
2025-06-02 01:01:27 +0700

    Config swagger

--- quiz-service/src/main/java/org/swd392/quizzes/config/SwaggerConfig.java
+++ quiz-service/src/main/java/org/swd392/quizzes/config/SwaggerConfig.java
@@ -4,38 +4,24 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI quizServiceOpenAPI() {
-        Server devServer = new Server();
-        devServer.setUrl("http://localhost:8802");
-        devServer.setDescription("Development Server");
-
-        Server prodServer = new Server();
-        prodServer.setUrl("https://api.personalityquiz.com");
-        prodServer.setDescription("Production Server");
-
-        Contact contact = new Contact();
-        contact.setEmail("admin@personalityquiz.com");
-        contact.setName("Quiz Service Team");
-
-        License license = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
-
-        Info info = new Info()
-                .title("Quiz Service API")
-                .version("1.0")
-                .contact(contact)
-                .description("API for managing personality quizzes, questions, and results")
-                .license(license);
-
-        return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Quiz Service API")
+                        .version("1.0.0")
+                        .description("API for managing personality quizzes, questions, and results")
+                        .contact(new Contact()
+                                .name("Quiz Service Team")
+                                .email("support@quizservice.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")));
     }
 }
