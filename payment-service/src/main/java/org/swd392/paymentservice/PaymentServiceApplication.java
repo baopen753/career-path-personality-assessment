4ed6ddfa691511e781ac3ec77a8e3e666918f7a7
baopen753 <tranquocbao070503@gmail.com>
2025-06-01 14:15:14 +0700

    config internal communication for payemnt service

--- payment-service/src/main/java/org/swd392/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/org/swd392/paymentservice/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package org.swd392.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
