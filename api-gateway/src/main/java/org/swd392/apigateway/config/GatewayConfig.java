6d2de1b773c0cae0a890a6ccf764c70f60b090cb
baopen753 <tranquocbao070503@gmail.com>
2025-06-03 17:54:41 +0700

    config: api gateway with customized filters

--- api-gateway/src/main/java/org/swd392/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/org/swd392/apigateway/config/GatewayConfig.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDateTime;
+
 @Configuration
 public class GatewayConfig {
 
@@ -13,11 +15,13 @@ public class GatewayConfig {
         return routeLocatorBuilder.routes()
                 .route(p -> p
                         .path("/swd391/quizzes/**")
-                        .filters(f -> f.rewritePath("/swd391/quizzes/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/swd391/quizzes/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://QUIZZES"))
                 .route(p -> p
                         .path("/swd391/seminars/**")
-                        .filters(f -> f.rewritePath("/swd391/seminars/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/swd391/seminars/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://SEMINARS"))
                 .build();
     }
