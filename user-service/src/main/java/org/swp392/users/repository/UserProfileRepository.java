0c9c6f0318a52c0344ee2bf01e072c3b78e7f0c8
HoangDev <hoangdmse184533@fpt.edu.vn>
2025-06-04 12:54:13 +0700

    user,profile-api

---
+++ user-service/src/main/java/org/swp392/users/repository/UserProfileRepository.java
@@ -0,0 +1,12 @@
+package org.swp392.users.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.swp392.users.entity.UserProfile;
+@Repository
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+    // Define custom query methods if needed
+    // For example, find by username or email
+
+}
