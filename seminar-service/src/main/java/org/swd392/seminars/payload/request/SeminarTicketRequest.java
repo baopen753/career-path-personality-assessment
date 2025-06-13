f1977051e26fd05a13d472cd434f94d6705dc24e
dt2401 <quanphse161574@fpt.edu.vn>
2025-06-13 12:59:31 +0700

    fix file seminarticket request and response

--- seminar-service/src/main/java/org/swd392/seminars/payload/request/SeminarTicketRequest.java
+++ seminar-service/src/main/java/org/swd392/seminars/payload/request/SeminarTicketRequest.java
@@ -1,9 +1,19 @@
 package org.swd392.seminars.payload.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 public class SeminarTicketRequest {
     private Integer seminarId;
+    private String description;
+    
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startingTime;
+    
+    @JsonIgnore
     private Integer userProfileId;
 } 
--- seminar-service/src/main/java/org/swd392/seminars/payload/response/SeminarTicketResponse.java
+++ seminar-service/src/main/java/org/swd392/seminars/payload/response/SeminarTicketResponse.java
@@ -1,12 +1,21 @@
 package org.swd392.seminars.payload.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
 public class SeminarTicketResponse {
     private Integer id;
-    private Integer seminarId; // Only return seminar ID instead of full seminar object
+    private Integer seminarId;
     private Integer userProfileId;
+    private String description;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime startingTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDateTime bookingTime;
+    
+    private boolean status;
 }
