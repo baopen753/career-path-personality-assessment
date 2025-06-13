08cbebc2cf81729ac1043d03c2df0c4baffe0a8d
dt2401 <quanphse161574@fpt.edu.vn>
2025-06-13 13:02:51 +0700

    edit  seminar-repo

--- seminar-service/src/main/java/org/swd392/seminars/repository/SeminarRepository.java
+++ seminar-service/src/main/java/org/swd392/seminars/repository/SeminarRepository.java
@@ -1,11 +1,14 @@
 package org.swd392.seminars.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.swd392.seminars.entity.Seminar;
 
 import java.util.List;
 
+@Repository
 public interface SeminarRepository extends JpaRepository<Seminar, Integer> {
-    List<Seminar> findByCreatedBy(Integer eventManagerId);
-    List<Seminar> findByStatus(String status);
+    List<Seminar> findByCreateBy(Integer createBy);
+    List<Seminar> findByStatus(Seminar.Status status);
+    List<Seminar> findByStatusApprove(Seminar.StatusApprove statusApprove);
 } 
