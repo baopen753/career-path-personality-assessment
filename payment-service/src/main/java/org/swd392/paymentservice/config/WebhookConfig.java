d2c14e4b821c422e06e82710a8fea8514ae0214a
baopen753 <tranquocbao070503@gmail.com>
2025-06-23 13:05:14 +0700

    modify: base uri webhook

--- payment-service/src/main/java/org/swd392/paymentservice/config/WebhookConfig.java
+++ payment-service/src/main/java/org/swd392/paymentservice/config/WebhookConfig.java
@@ -1,7 +1,7 @@
 package org.swd392.paymentservice.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import vn.payos.PayOS;
@@ -13,10 +13,13 @@ public class WebhookConfig implements CommandLineRunner {
 
     private final PayOS payOS;
 
+    @Value("${webhook.base-uri}")
+    private String webhookBaseUri;
+
     @Override
     public void run(String... args) throws Exception {
 
-        String webHookUrl = "https://8e6e-2402-800-63a8-b04a-c09f-9862-d003-5411.ngrok-free.app/payment/api/payments/webhook";
+        String webHookUrl = webhookBaseUri + "/payment/api/payments/webhook";
         try {
             String result = payOS.confirmWebhook(webHookUrl);
             System.out.println("✅ Webhook confirmed: " + result);
