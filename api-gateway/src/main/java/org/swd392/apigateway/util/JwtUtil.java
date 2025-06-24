790d3d95aaaa5eadd99435d9f3b68e91db1d3a37
baopen753 <tranquocbao070503@gmail.com>
2025-06-24 16:34:23 +0700

    fix: add filter for authN

---
+++ api-gateway/src/main/java/org/swd392/apigateway/util/JwtUtil.java
@@ -0,0 +1,50 @@
+package org.swd392.apigateway.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    // not for best practices
+    public static final String SECRET = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+
+    public boolean validateToken(final String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+            return true;  // valid token
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;  // invalid token
+        }
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    public Integer extractUserId(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.get("id", Integer.class);
+    }
+
+    public String extractUserRole(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.get("role", String.class);
+    }
+}
