2f55ed3964ece6231e00dd9ae91d5fb4ac70f8af
baopen753 <tranquocbao070503@gmail.com>
2025-07-09 14:48:28 +0700

    modify: received saga event

--- notification-service/src/main/java/org/swd392/notification/event/impl/TicketBookedListener.java
+++ notification-service/src/main/java/org/swd392/notification/event/impl/TicketBookedListener.java
@@ -30,10 +30,6 @@ public class TicketBookedListener implements EventListener<TicketBookedEvent> {
     @Override
     public void consume(TicketBookedEvent event) {
         log.info("TicketBookedEvent received: {}", event);
-        // LOG INFO: TicketBookedEvent received: TicketBookedEvent(userId=7, email=[user-email], fullName=...., paymentOrderCode=1751594894132, status=COMPLETED, createdAt=2025-07-04T10:38:09.389508823)
-
-        // TODO: gửi email thông email dưới đây đi kèm thông tin liên quan đến transaction
-
 
         try {
             // Create email context
